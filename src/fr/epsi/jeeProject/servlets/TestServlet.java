4a77d3023727cbc77427ab7980b28e300a1e4674
Fabien RENAUD <frd.frenaud@gmail.com>
2019-03-29 09:02:46 +0100

    TestJSP
    a

--- src/fr/epsi/jeeProject/servlets/TestServlet.java
+++ src/fr/epsi/jeeProject/servlets/TestServlet.java
@@ -27,7 +27,8 @@ public class TestServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        Logger.info("Je suis dans ma Servlet");
+        request.getRequestDispatcher("TestJSP.jsp").forward(request, response);
     }
 
     /**
---
+++ web/TestJSP.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="ISO-8859-1">
+    <title>My JSP Page</title>
+</head>
+<body>
+<% out.println("Coucou"); %>
+</body>
+</html>
